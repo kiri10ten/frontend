723f6a2edcc0e3c5e263172e3894af3dfa20f7b5
kiri10ten <126086612+kiri10ten@users.noreply.github.com>
2023-10-11 10:21:06 +0530

    responsive design update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { useUserStore } from '@/stores/user'
 
+
+let shownav= ref(false)
 const userStore = useUserStore()
 
 function logout() {
@@ -12,7 +14,7 @@ function logout() {
     <div >
 
        
-        <nav class="pt-4 pr-8 pl-8 pb-4 flex items-center justify-between  bg-nav-grey">
+        <nav class="pt-4 pr-8 pl-8 pb-4 flex items-center max-sm:hidden justify-between w-full bg-nav-grey">
             <NuxtLink to="/" class="text-lg text-lime-400">University Archive</NuxtLink>
 
             <div class="flex items-center text-sm space-x-4">
@@ -33,6 +35,32 @@ function logout() {
                 </div>
             </div>
         </nav>
+
+         <nav class="pt-4 pr-8 pl-8 pb-4 flex sm:hidden max-sm:flex-col  gap-5 justify-between w-full bg-nav-grey">
+              <div class=" flex flex-row justify-between"> 
+            <NuxtLink to="/" class="text-lg text-lime-400">University Archive</NuxtLink>
+            <svg  @click="shownav=!shownav" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+    </svg>
+
+            </div> 
+                
+                <div v-if="shownav" class="flex flex-col gap-5 ">
+                    <NuxtLink to="/" class=" hover:text-lime-200 text-gray-400">Home</NuxtLink>
+                    <NuxtLink to="/browse" class=" hover:text-lime-200 text-gray-400">Browse</NuxtLink>
+                    <NuxtLink to="/myarchives" class="  hover:text-lime-200  text-gray-400 rounded-xl">My archive</NuxtLink>
+                    <NuxtLink to="/createarchive" class="   hover:text-lime-200  text-gray-400 pr-72 rounded-xl">Create archive</NuxtLink>
+                        <template class="flex flex-row gap-3" v-if="userStore.user.isAuthenticated">
+                           <div ><a v-on:click="logout" class=" hover:text-lime-200 border-b-lime-900 hover:border-lime-900 border-2  p-1 pl-3 pr-3 text-gray-400 rounded-xl">Log out</a>
+                </div>
+                        </template>
+                        <template v-else class="">
+                            <NuxtLink to="/login" class="  hover:text-lime-200  text-gray-400 ">Log in</NuxtLink>
+                            <NuxtLink to="/signup" class="hover:text-lime-200  text-gray-400  ">Sign up</NuxtLink>
+                        </template>
+                    </div>
+                
+            </nav>
        
 
         <slot />
